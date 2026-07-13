c5485c99997b687011cdc8e207adf547787fd2a1
Moritz <moritz@krawczynski.de>
2026-07-13 18:19:33 +0200

    Add member - flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import member
 import trainer
 import helper
@@ -30,10 +30,31 @@ def courses():
     courses = course.get_courses_room()
     return render_template("courses.html", courses=courses)
 
+
 @app.route("/rooms")
 def rooms():
     rooms = room.get_rooms()
     return render_template("rooms.html", rooms=rooms)
+
+
+@app.route("/add_member", methods=["GET", "POST"])
+def add_member():
+    if request.method == "POST":
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        trainerid = request.form["trainerid"]
+
+        member.insert_member(
+            firstname,
+            lastname,
+            email,
+            phone,
+            trainerid
+        )
+        return redirect(url_for("members"))
+    return render_template("add_member.html")
 
 
 if __name__ == "__main__":
